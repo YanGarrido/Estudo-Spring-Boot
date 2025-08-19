b04f69ab48972451b49c428f5765209c8d0db32b
Yan <yangarridodev@gmail.com>
2025-08-19 08:18:14 -0300

    Primeiro projeto com Spring(Estudos)

---
+++ src/main/java/com/example/meu_primeiro_springboot/model/Produto.java
@@ -0,0 +1,42 @@
+package com.example.meu_primeiro_springboot.model;
+
+import jakarta.persistence.*;
+
+@Entity // indica que essa  Classe é uma Entidade JPA
+@Table(name ="produtos") // Indica e defini o nome da tabela
+public class Produto {
+  @Id // indica que isso é um ID
+  @GeneratedValue(strategy = GenerationType.IDENTITY)// indica que o ID vai ser uma PK gerada automaticamente
+  private Long id;
+
+  private String name;
+  private Double preco;
+
+  public Produto () {}
+
+  public Produto (String name, Double preco){
+    this.name = name;
+    this.preco = preco;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Double getPreco() {
+    return preco;
+  }
+
+  public void setPreco(Double preco) {
+    this.preco = preco;
+  }
+}
+
